6de5ea78f0a9c86a6deb67b6818bf68daf011f18
Nagarjun <w3.nagarjuna@gmail.com>
2020-09-19 13:28:06 +0530

    app component updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, NavigationStart, Router } from '@angular/router';
+import { Observable, merge } from 'rxjs';
+import { filter, mapTo } from "rxjs/operators";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ngrx-checklist';
+export class AppComponent implements OnInit{
+  public loading$: Observable<boolean>;
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    const navigationStart$ = this.router.events.pipe(
+      filter(event => event instanceof NavigationStart),
+      mapTo(true)
+    );
+
+    const navigationEnd$ = this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd),
+      mapTo(false)
+    );
+
+    this.loading$ = merge(navigationStart$, navigationEnd$);
+  }
 }
--- src/app/projects/projects-list/projects-list.component.ts
+++ src/app/projects/projects-list/projects-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-projects-list',
+  selector: 'nc-projects-list',
   templateUrl: './projects-list.component.html',
   styleUrls: ['./projects-list.component.scss']
 })
