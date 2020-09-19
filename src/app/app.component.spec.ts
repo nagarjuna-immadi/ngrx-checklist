3c90924ab95de66c67cdf05072f1e14069abf76a
Nagarjun <w3.nagarjuna@gmail.com>
2020-09-19 13:00:56 +0530

    updated name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ngrx-poc'`, () => {
+  it(`should have as title 'ngrx-checklist'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ngrx-poc');
+    expect(app.title).toEqual('ngrx-checklist');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ngrx-poc app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('ngrx-checklist app is running!');
   });
 });
